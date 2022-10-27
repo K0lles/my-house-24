0c688c000af739555df16bfe124993d16bfbf342
K0lles <oleksijkolotilo63@gmail.com>
2022-10-27 20:28:53 +0300

    created validation in tariffs and improved main validating functions

--- administrator_panel/static/scripts.js
+++ administrator_panel/static/scripts.js
@@ -1,41 +1,68 @@
 function fieldIsValid(element) {
     if (element.tagName === 'SELECT') {
-        selectElementValidation(element);
-        return null;
+        return selectElementValidation(element);
+    }
+    if (element.tagName === 'TEXTAREA') {
+        return textTypeValidation(element);
     }
     let dict_function_by_type = {
         'text': textTypeValidation,
+        'number': numberTypeValidation
     }
-    dict_function_by_type[$(`#${element.id}`).attr('type')](element);
+    return dict_function_by_type[$(`#${element.id}`).attr('type')](element);
 }
 
 function textTypeValidation(element) {
     if (element.value !== '') {
         element.style = 'border: 1px solid #58c76d';
         $(`#${element.id}-errors`).text('');
-        return null;
+        return true;
     }
     else if (element.value === '') {
         if (!($(`#${element.id}-errors`).text()).includes('Поле не може бути пустим')) {
             $(`#${element.id}-errors`).text('Поле не може бути пустим');
             element.style = 'border: 1px solid red';
-            return null;
+            return false;
         }
 
     }
 }
 
 function selectElementValidation(element) {
-    if (!$(`#${element.id} option:selected`).attr('value')) {
+    if (!$(`#${element.id} option:selected`).attr('value') || $(`#${element.id} option:selected`).attr('value') === 'none') {
         if (!$(`${element.id}-errors`).text().includes('Виберіть од. вим.')) {
             $(`#${element.id}-errors`).text('Виберіть од. вим.');
         }
         element.style = 'border: 1px solid red';
+        return false;
     }
     else {
         element.style = 'border: 1px solid #58c76d';
         $(`#${element.id}-errors`).text('');
+        return true;
     }
-    return null;
+}
+
+function numberTypeValidation(element) {
+
+    if (element.value !== '' && element.value > 0.00) {
+        element.style = 'border: 1px solid #58c76d';
+        $(`#${element.id}-errors`).text('');
+        return true;
+    }
+    else if (element.value === '') {
+        if (!($(`#${element.id}-errors`).text()).includes('Поле не може бути пустим')) {
+            $(`#${element.id}-errors`).text('Поле не може бути пустим');
+            element.style = 'border: 1px solid red';
+            return false;
+        }
+    }
+    else if (element.value <= 0.00) {
+        if (!($(`#${element.id}-errors`).text()).includes('Ціна не може бути меншою 0')) {
+            $(`#${element.id}-errors`).text('Ціна не може бути меншою 0');
+            element.style = 'border: 1px solid red';
+            return false;
+        }
+    }
 
 }

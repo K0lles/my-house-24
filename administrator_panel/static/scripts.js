4b694f8e204e830b4bfd2449814623336821ffca
K0lles <oleksijkolotilo63@gmail.com>
2022-10-27 22:52:18 +0300

    added validation for user creation and updating

--- administrator_panel/static/scripts.js
+++ administrator_panel/static/scripts.js
@@ -7,7 +7,9 @@ function fieldIsValid(element) {
     }
     let dict_function_by_type = {
         'text': textTypeValidation,
-        'number': numberTypeValidation
+        'number': numberTypeValidation,
+        'email': emailTypeValidation,
+        'password': passwordTypeValidation
     }
     return dict_function_by_type[$(`#${element.id}`).attr('type')](element);
 }
@@ -30,8 +32,8 @@ function textTypeValidation(element) {
 
 function selectElementValidation(element) {
     if (!$(`#${element.id} option:selected`).attr('value') || $(`#${element.id} option:selected`).attr('value') === 'none') {
-        if (!$(`${element.id}-errors`).text().includes('Виберіть од. вим.')) {
-            $(`#${element.id}-errors`).text('Виберіть од. вим.');
+        if (!$(`${element.id}-errors`).text().includes('Виберіть елемент')) {
+            $(`#${element.id}-errors`).text('Виберіть елемент');
         }
         element.style = 'border: 1px solid red';
         return false;
@@ -66,3 +68,88 @@ function numberTypeValidation(element) {
     }
 
 }
+
+function emailTypeValidation(element) {
+    let emailValid;
+    emailValid = textTypeValidation(element)
+    if (!emailValid) return emailValid;
+    if (!(element.value).match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        $(`#${element.id}-errors`).text('Неправильно введений email');
+        element.style = 'border: 1px solid red';
+        return false;
+    }
+
+    else {
+        element.style = 'border: 1px solid #58c76d';
+        $(`#${element.id}-errors`).text('');
+        return true;
+    }
+
+}
+
+function passwordTypeValidation(element) {
+    if (element.id === 'id_repeat-password') {
+        console.log('we are here')
+        if ($('#id_password').val() !== element.value) {
+            if (!($(`#id_password-errors`).text()).includes('Паролі повинні співпадати')) {
+                $(`#id_password-errors`).text('Паролі повинні співпадати');
+                $('#id_password').css('border', '1px solid red');
+            }
+            return false;
+        }
+        else if ($('#id_password').val() === element.value && element.value !== '') {
+            $('#id_password').css('border', '1px solid #58c76d');
+            $(`#id_password-errors`).text('');
+            return true;
+        }
+    }
+    else {
+        if (element.value !== '' && $('id_repeat-password').val() !== '' && $('#id_repeat-password').val() === element.value) {
+            element.style = 'border: 1px solid #58c76d';
+            $(`#${element.id}-errors`).text('');
+            return true;
+        } else if (element.value === '') {
+            if (!($(`#${element.id}-errors`).text()).includes('Поле не може бути пустим')) {
+                $(`#${element.id}-errors`).text('Поле не може бути пустим');
+                element.style = 'border: 1px solid red';
+            }
+            return false;
+        } else if ($('#id_repeat-password').val() !== element.value) {
+            if (!($(`#${element.id}-errors`).text()).includes('Паролі повинні співпадати')) {
+                $(`#${element.id}-errors`).text('Паролі повинні співпадати');
+                element.style = 'border: 1px solid red';
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
+function validatePasswordUpdate(element) {
+    if (element.id === 'id_repeat-password') {
+        console.log('we are here')
+        if ($('#id_password').val() !== element.value) {
+            if (!($(`#id_password-errors`).text()).includes('Паролі повинні співпадати')) {
+                $(`#id_password-errors`).text('Паролі повинні співпадати');
+                $('#id_password').css('border', '1px solid red');
+            }
+            return false;
+        }
+    }
+    else {
+        if ($('#id_repeat-password').val() !== element.value) {
+            if (!($(`#${element.id}-errors`).text()).includes('Паролі повинні співпадати')) {
+                $(`#${element.id}-errors`).text('Паролі повинні співпадати');
+                element.style = 'border: 1px solid red';
+            }
+            return false;
+        }
+    }
+    $('#id_password').css('border', '1px solid #58c76d');
+    $(`#id_password-errors`).text('');
+    return true;
+}
+
+$('tr[data-href]').on('click', function() {
+    document.location = $(this).data('href');
+});
